849567c8fbdb20aac964270d4694e1d1d294ae42
jayeshkrt <jayeshkrt@gmail.com>
2022-07-31 12:26:15 +0530

    Arrays II problem 2

---
+++ 2.2_merge_intervals.py
@@ -0,0 +1,39 @@
+class Solution:
+    def merge(self, intervals):
+
+        # sort the input array
+        intervals.sort()
+
+        # declare answer list
+        ans = []
+
+        # declare length of i/p
+        n = len(intervals)
+
+        # run a for on each element of the input
+        for i in range(n):
+
+            # declare start and end
+            start = intervals[i][0]
+            end = intervals[i][1]
+
+            # check if o/p is not zero
+            if len(ans) != 0:
+                # check if start is less than the last interval's first value
+                if start <= ans[-1][1]:
+                    continue
+
+            # check intervals lying ahead of present
+            for j in range(i+1, n):
+                if intervals[j][0] <= end:
+                    end = max(end, intervals[j][1])
+
+            end = max(end, intervals[i][1])
+            ans.append([start, end])
+
+        return ans
+
+test = [[1,3],[2,6],[8,10],[15,18]]
+sol = Solution()
+print(sol.merge(test))
+
